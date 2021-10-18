8c6bb50847c35a0c32a29f01dec7327d6afca50f
cmountford <c.mountford@boardex.com>
2021-10-18 15:04:35 +0100

    Adding in json property names

--- Tfl.Road.AppServices/Models/TflRoadEntity.cs
+++ Tfl.Road.AppServices/Models/TflRoadEntity.cs
@@ -1,16 +1,27 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Net;
+using System.Text.Json.Serialization;
 
 namespace Tfl.Road.AppServices.Models
 {
     public class TflRoadEntity
     {
+        [JsonPropertyName("$type")]
         public string Type { get; set; }
+        [JsonPropertyName("id")]
         public string Id { get; set; }
+        [JsonPropertyName("displayName")]
         public string DisplayName { get; set; }
+        [JsonPropertyName("statusSeverity")]
         public string StatusSeverity { get; set; }
+        [JsonPropertyName("statusSeverityDescription")]
         public string StatusSeverityDescription { get; set; }
+        [JsonPropertyName("bounds")]
         public string Bounds { get; set; }  // TODO Make these list of Coordinates
+        [JsonPropertyName("envelope")]
         public string Envelope { get; set; }  // TODO Make these list of Coordinates
+        [JsonPropertyName("url")]
         public string Url { get; set; }
     }
 }
