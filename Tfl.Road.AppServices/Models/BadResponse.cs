c43df018073daea11a4220be3c857e4ddfb1eef3
cmountford <c.mountford@boardex.com>
2021-10-18 15:05:32 +0100

    And for the 404 response model

--- Tfl.Road.AppServices/Models/BadResponse.cs
+++ Tfl.Road.AppServices/Models/BadResponse.cs
@@ -2,17 +2,26 @@
 using System.Collections.Generic;
 using System.Net;
 using System.Text;
+using System.Text.Json.Serialization;
 
 namespace Tfl.Road.AppServices.Models
 {
     public class BadResponse
     {
+        [JsonPropertyName("$type")]
         public string Type { get; set; }
+        public string Url { get; set; }
+        [JsonPropertyName("timestampUtc")]
         public DateTime TimeStampUtc { get; set; }
+        [JsonPropertyName("exceptionType")]
         public string ExceptionType { get; set; }
-        public HttpStatusCode HttpStatusCode  { get; set; }
+        [JsonPropertyName("httpStatusCode")]
+        public HttpStatusCode HttpStatusCode { get; set; }
+        [JsonPropertyName("httpStatus")]
         public string HttpStatus { get; set; }
+        [JsonPropertyName("relativeUri")]
         public string RelativeUri { get; set; }
+        [JsonPropertyName("message")]
         public string Message { get; set; }
     }
 }
